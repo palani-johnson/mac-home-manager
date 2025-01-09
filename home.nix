785a237ba82413aba8528f1cb63bc12377b77c6b
Palani Johnson <palani.johnson@fbgpg.com>
2025-01-08 17:22:07 -0700

    migrated to home manager

---
+++ home.nix
@@ -0,0 +1,85 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  sessionVariables = {
+    EDITOR = "code";
+  };
+in {
+  home = {
+    username = "pjohnso3";
+    homeDirectory = "/Users/pjohnso3";
+    stateVersion = "24.11";
+    packages = with pkgs; [
+      nixd
+      alejandra
+      imagemagick
+      docker
+      python3
+    ];
+
+    sessionVariables = sessionVariables;
+    preferXdgDirectories = true;
+  };
+
+  xdg.enable = true;
+
+  programs = {
+    home-manager.enable = true;
+
+    zsh = {
+      enable = true;
+      sessionVariables = sessionVariables;
+      dotDir = ".config/zsh";
+      history.path = "$HOME/.cache/zsh/history";
+      initExtra = builtins.readFile ./zsh/initExtra.zsh;
+      enableCompletion = true;
+      prezto.enable = true;
+    };
+
+    nushell = {
+      enable = true;
+      extraConfig = builtins.readFile ./nushell/extraConfig.nu;
+    };
+
+    git = {
+      enable = true;
+      userName = "Palani Johnson";
+      userEmail = "palanijohnson@gmail.com";
+      extraConfig.init.defaultBranch = "main";
+    };
+
+    starship = {
+      enable = true;
+      enableNushellIntegration = true;
+      enableZshIntegration = true;
+      settings = {
+        "$schema" = "https://starship.rs/config-schema.json";
+
+        format = "$username$hostname$directory$fill$git_branch$git_state$git_status$nix_shell$cmd_duration$line_break$sudo$status$shell$character";
+
+        fill.disabled = false;
+        fill.symbol = " ";
+        shell.disabled = false;
+        cmd_duration.format = "[$duration]($style) ";
+        git_branch.format = "[$symbol$branch(:$remote_branch)]($style) ";
+        nix_shell.format = "[ $name]($style) ";
+      };
+    };
+
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+      enableNushellIntegration = true;
+      enableZshIntegration = true;
+      silent = true;
+    };
+
+    carapace = {
+      enable = true;
+      enableNushellIntegration = true;
+      enableZshIntegration = true;
+    };
+  };
+}
